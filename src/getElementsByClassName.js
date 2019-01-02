431c0b429b96e8830200e9541a1a95d40ad35a4e
DSaquin <dawnsaq@gmail.com>
2019-01-02 13:26:24 -0800

    recursion review for getElementByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // create a list
+  var result = []
+
+  // create a function will use recurssion on
+  function dawn (node) {
+      // we will know what is the node
+      // check if the node has the class name
+      if (node.classList.contains(className)) {
+        // if it does has the class add it to the list
+        // take a list push
+        result.push(node)
+      }
+      // loop through the node children
+      console.log(node.children);
+      for (let i = 0; i < node.children.length; i++) {
+        // we are going to invoke it , extra step.. overall
+
+        dawn(node.children[i])
+      }
+    }
+  dawn(document.body);
+  // return a list
+  return result;
 };
